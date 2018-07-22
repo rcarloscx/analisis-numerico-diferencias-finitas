1c3fcc1340b860e5dd9101419f4c902544bc1beb
Roberto Carlos <rcarloscx@gmail.com>
2018-07-21 22:37:55 -0600

    Clase Diferencias finitas donde se codificara las diferentes diferencias finitas existentes junto con sus variables y metodos get y set

---
+++ src/main/java/rc/analisisnumerico/derivacion/tools/DiferenciasFinitas.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rc.analisisnumerico.derivacion.tools;
+
+/**
+ *
+ * @author rcarlos
+ */
+public class DiferenciasFinitas {
+
+    private double h;
+    private final Numerador numerador;
+
+    public DiferenciasFinitas(double x, double h, Funcion fun) {
+        this.h = h;
+        numerador = new Numerador(x, h, fun);
+    }
+
+    public Funcion getFuncion() {
+        return this.numerador.getFuncion();
+    }
+
+    public void setFuncion(Funcion funcion) {
+        this.numerador.setFuncion(funcion);
+    }
+
+    public double getXo() {
+        return numerador.getXo();
+    }
+
+    public void setXo(double xo) {
+        this.numerador.setXo(xo);
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+        this.numerador.setH(h);
+    }
+
+}
