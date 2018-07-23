38e0ebeee5ec40130f58c0b5284783c249a3e289
Roberto Carlos <rcarloscx@gmail.com>
2018-07-22 19:08:48 -0600

    Primeras y Segundas diferencias finitas hacia adelante

--- src/main/java/rc/analisisnumerico/derivacion/tools/DiferenciasFinitas.java
+++ src/main/java/rc/analisisnumerico/derivacion/tools/DiferenciasFinitas.java
@@ -27,6 +27,71 @@ public class DiferenciasFinitas {
         return numerador.reducir() / h;
     }
 
+    public double AdelantePrimeraOrden2() {
+        numerador.removeAll();
+        numerador.add(1, 2);
+        numerador.add(-2, 1);
+        numerador.add(1, 0);
+        return numerador.reducir() / (Math.pow(h, 2));
+    }
+
+    public double AdelantePrimeraOrden3() {
+        numerador.removeAll();
+        numerador.add(1, 3);
+        numerador.add(-3, 2);
+        numerador.add(3, 1);
+        numerador.add(-1, 0);
+        return numerador.reducir() / (Math.pow(h, 3));
+    }
+
+    public double AdelantePrimeraOrden4() {
+        numerador.removeAll();
+        numerador.add(1, 4);
+        numerador.add(-4, 3);
+        numerador.add(6, 2);
+        numerador.add(-4, 1);
+        numerador.add(1, 0);
+        return numerador.reducir() / (Math.pow(h, 2));
+    }
+
+    public double AdelanteSegundaOrden1() {
+        numerador.removeAll();
+        numerador.add(-1, 2);
+        numerador.add(4, 1);
+        numerador.add(-3, 0);
+        return numerador.reducir() / ((2) * (h));
+    }
+
+    public double AdelanteSegundaOrden2() {
+        numerador.removeAll();
+        numerador.add(-1, 3);
+        numerador.add(4, 2);
+        numerador.add(-5, 1);
+        numerador.add(2, 0);
+        return numerador.reducir() / (Math.pow(h, 2));
+    }
+
+    public double AdelanteSegundaOrden3() {
+        numerador.removeAll();
+        numerador.add(-3, 4);
+        numerador.add(14, 3);
+        numerador.add(-24, 3);
+        numerador.add(18, 1);
+        numerador.add(-5, 0);
+        return numerador.reducir() / (Math.pow(h, 3));
+    }
+
+    public double AdelanteSegundaOrden4() {
+        numerador.removeAll();
+        numerador.add(-2, 5);
+        numerador.add(11, 4);
+        numerador.add(-24, 3);
+        numerador.add(26, 2);
+        numerador.add(-14, 1);
+        numerador.add(3, 0);
+        return numerador.reducir() / (Math.pow(h, 4));
+    }
+    
     public Funcion getFuncion() {
         return this.numerador.getFuncion();
     }
