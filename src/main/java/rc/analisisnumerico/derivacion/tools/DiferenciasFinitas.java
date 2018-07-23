cb5db58ae1b7e3b56f5797aededf662d0b0aab45
Roberto Carlos <rcarloscx@gmail.com>
2018-07-22 20:00:23 -0600

    Metodos que calculan las primeras y segundas diferencias finitas hacia atras

--- src/main/java/rc/analisisnumerico/derivacion/tools/DiferenciasFinitas.java
+++ src/main/java/rc/analisisnumerico/derivacion/tools/DiferenciasFinitas.java
@@ -91,7 +91,80 @@ public class DiferenciasFinitas {
         numerador.add(3, 0);
         return numerador.reducir() / (Math.pow(h, 4));
     }
-    
+
+    //Diferencias finitas hacia Atras
+    public double atrasPrimeraOrden1() {
+        numerador.removeAll();
+        numerador.add(1, 0);
+        numerador.add(-1, 1);
+        return numerador.reducir() / (h);
+    }
+
+    public double atrasPrimeraOrden2() {
+        numerador.removeAll();
+        numerador.add(1, 0);
+        numerador.add(-2, -1);
+        numerador.add(1, -2);
+        return numerador.reducir() / (Math.pow(h, 2));
+    }
+
+    public double atrasPrimeraOrden3() {
+        numerador.removeAll();
+        numerador.add(1, 0);
+        numerador.add(-3, -1);
+        numerador.add(3, -2);
+        numerador.add(-1, -3);
+        return numerador.reducir() / (Math.pow(h, 3));
+    }
+
+    public double atrasPrimeraOrden4() {
+        numerador.removeAll();
+        numerador.add(1, 0);
+        numerador.add(-4, -1);
+        numerador.add(6, -2);
+        numerador.add(-4, -3);
+        numerador.add(1, -4);
+        return numerador.reducir() / (Math.pow(h, 4));
+    }
+
+    public double atrasSegundaOrden1() {
+        numerador.removeAll();
+        numerador.add(3, 0);
+        numerador.add(-4, -1);
+        numerador.add(1, -2);
+        return numerador.reducir() / ((2) * (h));
+    }
+
+    public double atrasSegundaOrden2() {
+        numerador.removeAll();
+        numerador.add(2, 0);
+        numerador.add(-5, -1);
+        numerador.add(4, -2);
+        numerador.add(-1, -3);
+        return numerador.reducir() / (Math.pow(h, 2));
+    }
+
+    public double atrasSegundaOrden3() {
+        numerador.removeAll();
+        numerador.add(5, 0);
+        numerador.add(-18, -1);
+        numerador.add(24, -2);
+        numerador.add(-14, -3);
+        numerador.add(3, -4);
+        return numerador.reducir() / (Math.pow(h, 3));
+    }
+
+    public double atrasSegundaOrden4() {
+        numerador.removeAll();
+        numerador.add(3, 0);
+        numerador.add(-14, -1);
+        numerador.add(26, -2);
+        numerador.add(-24, -3);
+        numerador.add(11, -4);
+        numerador.add(-2, -5);
+        return numerador.reducir() / (Math.pow(h, 4));
+    }
+
     public Funcion getFuncion() {
         return this.numerador.getFuncion();
     }
